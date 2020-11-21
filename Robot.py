112e8e5fd8580555835ba536b7d3dbfdbb11e8ce
bagleric <bagleric@outlook.com>
2020-11-21 09:01:25 -0700

    update robot file

--- Robot.py
+++ Robot.py
@@ -2,8 +2,8 @@ import json
 from math import sqrt, acos, pi, cos, sin
 
 
-class snowblower:
-    def __init__(self, x = 0.0, y = 0.0, heading = 0.0, velocity = 0.0, distance = 0.0):
+class Robot:
+    def __init__(self, x=0.0, y=0.0, heading=0.0, velocity=0.0, distance=0.0):
         self.heading = heading
         self.position = [x, y]
         self.velocity = velocity
@@ -12,27 +12,31 @@ class snowblower:
         self.fullyCoveredBool = False
         self.wait = False
 
-    def drive_straight(self, distance_mm = 0.0, speed_mmps = 0.0):
+    def drive_straight(self, distance_mm=0.0, speed_mmps=0.0):
         self.distance = distance_mm
         self.velocity = speed_mmps
-        
+
         list_of_points_to_traverse = []
 
-        for x in range(0,distance_mm):
-            list_of_points_to_traverse.append([x*cos(self.heading), x*sin(self.heading)])
+        for x in range(0, distance_mm):
+            list_of_points_to_traverse.append(
+                [x*cos(self.heading), x*sin(self.heading)])
 
         return list_of_points_to_traverse
 
-    def turn_in_place(self, degrees = 0.0):
-        #TODO: Simulate turning
+    def turn_in_place(self, degrees=0.0):
+        # TODO: Simulate turning
         self.heading += degrees
 
     def distance_mm(self, dist):
         return dist
+
     def speed_mmps(self, speed):
         return speed
+
     def degrees(self, angle):
         return angle
+
 
 """
     # drive in a straight line 100 mm at a speed of 30 mm/s
@@ -41,4 +45,3 @@ class snowblower:
     # turn the robot 90 degrees
     robot.behavior.turn_in_place(degrees(90))
 """
-    
